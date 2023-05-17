6fc45983b276cc6084f4841e03bc60ca8db8dc20
poke1896 <adrian.host96@gmail.com>
2023-05-17 11:48:04 -0600

    Vercel test

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Heading = () => {
-  return <div className="text-center text-4xl font-bold">Ye's ToDoList</div>;
+  return <div className="text-center text-4xl font-bold">ToDoList</div>;
 };
 
 export default Heading;
